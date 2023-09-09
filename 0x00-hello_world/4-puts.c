42b096a92505f504a7bd484a1a79b0d2091641c6
Ahmedelsayed2003 <sobhyahmed325@gmail.com>
2023-09-09 03:42:41 -0700

    aaaa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main -entry poin
+ *
+ * description: A c program that prints with puts function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
